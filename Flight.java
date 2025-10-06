6fe98b01489538c622b0bb9efb6f449c7f9ff4a2
Pasindu <madubashap98@gmail.com>
2025-10-06 19:52:48 +0530

    updated code

--- Flight.java
+++ Flight.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Flight {
+    private String FlightNumber;
+    private String destination;
+    private String departureTime;
+    private double Price;
+    ArrayList<Ticket> tickets;
+
+    public Flight(String FlightNumber,String departureTime,String destination,double Price){
+        tickets=new ArrayList<>();
+        this.FlightNumber=FlightNumber;
+        this.destination=destination;
+        this.departureTime=departureTime;
+        this.Price=Price;
+    }
+
+    public String getFlightNumber() {
+        return FlightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
 }
--- Passenger.java
+++ Passenger.java
@@ -1,4 +1,18 @@
 public class Passenger extends Person{
+   private String passportNumber;
+   public Passenger(String name,String ID,String passportNumber){
+       super(name,ID);
+       this.passportNumber=passportNumber;
+   }
 
+   public String getPassportNumber(){
+       return passportNumber;
+   }
+
+
+   @Override
+    public void showInfo(){
+       System.out.println("Name of Passanger: "+getName()+"\n"+"ID number of passenger: "+getID()+"\n"+"passport number: "+getPassportNumber());
+   }
 
 }
